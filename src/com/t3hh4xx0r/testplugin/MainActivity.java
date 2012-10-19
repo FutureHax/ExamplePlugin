7903609d025911e9b3907ce77c89bc86d512a09b
r2DoesInc <r2doesinc@gmail.com>
2012-10-19 02:14:23 -0400

    UPdate example plugin

--- src/com/t3hh4xx0r/testplugin/MainActivity.java
+++ src/com/t3hh4xx0r/testplugin/MainActivity.java
@@ -8,21 +8,24 @@ import android.os.Bundle;
 import android.view.Window;
 
 public class MainActivity extends Activity {
-	//This must be the name f the preferences file
-    public static final String PREFERENCES_KEY = "plugin_preferences";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+	
 
 		//you need some activity so that the broadcat intents can be registered.
 		//If you want to put a little "about" page with your info here, you can go ahead and do that.
 		//You also need to build your sharedpreferences here with the MODE WORLD WRITABLE option.
 		//The "shouldshow" should be set here by defaul to true. 
-		final SharedPreferences prefs = this.getSharedPreferences(PREFERENCES_KEY, Context.MODE_WORLD_WRITEABLE);
+		final SharedPreferences prefs = this.getSharedPreferences(getPrefsKey(this ), Context.MODE_WORLD_WRITEABLE);
 		if (!prefs.getBoolean("shouldShow", false)) {
 			prefs.edit().putBoolean("shouldShow", true).apply();
-		}
+		}			
+	}
+	
+	public static String getPrefsKey(Context c) {
+		return c.getPackageName()+"_plugin_preferences";
 	}
 }
